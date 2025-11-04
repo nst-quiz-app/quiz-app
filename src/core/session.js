1c6fdf4073e968ab850103d080b5554c1d99642c
JOTHINKUMAR B <nst@joth.in>
2025-11-04 16:02:42 +0530

    session manager

---
+++ src/core/session.js
@@ -0,0 +1,20 @@
+class SessionManager {
+    constructor() {
+        this.username = sessionStorage.getItem("username")
+    }
+    isLoggedIn() {
+        return !this.username
+    }
+    isAdmin() {
+        return this.username === "admin"
+    }
+    login(username) {
+        this.username = username
+        sessionStorage.setItem("username", username)
+    }
+    logout() {
+        this.username = null
+        sessionStorage.removeItem("username")
+    }
+}
+export {SessionManager}
